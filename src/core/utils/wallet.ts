654b203c137387e10b8231af6bf83595e0b6ef99
Govard Barkhatov <gbarkhatov@gmail.com>
2025-01-22 19:25:29 +0200

    wip address length

--- src/core/utils/wallet.ts
+++ src/core/utils/wallet.ts
@@ -2,6 +2,19 @@ import { networks } from "bitcoinjs-lib";
 
 import { Network } from "@/core/types";
 
+// 62 42 35 34
+// Signet BTC
+// Taproot tb1pkyuzc4w2lwxkcl9lvjl9ny24pduxg8nhjfv7epan5r7kszfky6gsqqn0wk
+// Native SegWit tb1q2pgynqumcvhcd9vs4hzk2rzcfctuj9lu68nd57
+// Nested SegWit 2N2kXB3LWH5uToJXN2QuYpiivFmbeavisDz
+// Legacy mtAqM5GcvNkuJeSpkTm6KPWjCjWTPTVJoK
+
+// Mainnet BTC
+// Taproot bc1p8gvwe02v6m9juqxwq0wvshdtrzamd43hjse4p73an3mlvhkg3uqs7lusak
+// Native SegWit bc1qej9hffyx4nt5y7g9s35r5r4nkp7adanraahlu4
+// Nested SegWit 3EKYctto5ziebQ6nuEKhknZLjiJCDSfWCX
+// Legacy 147ufF7p5j6fm2LazuZ1dYh8RYhP4TGAem
+
 export function validateAddressNetwork(network: Network, address: string): void {
   if (network === Network.MAINNET && !address.startsWith("bc1")) {
     throw new Error("Incorrect address prefix for Mainnet. Expected address to start with 'bc1'.");
